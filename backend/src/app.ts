a4ad56a6a0e577f119e01c62843cf1f5d5fd832f
Gabriel Brun <lydstyl@gmail.com>
2025-09-19 00:33:43 +0200

    Fix cors on dev.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,8 +15,14 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
+      'http://localhost:5175',
+      'http://localhost:5176',
+      'http://localhost:5177',
       'http://127.0.0.1:5173',
-      'http://127.0.0.1:5174'
+      'http://127.0.0.1:5174',
+      'http://127.0.0.1:5175',
+      'http://127.0.0.1:5176',
+      'http://127.0.0.1:5177'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
